b01706f4db0270181dc025fc3317518c6fbe6c58
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-03-09 19:04:26 +0530

    Time: 3 ms (75.27%) | Memory: 48.3 MB (20.04%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,14 +1,39 @@
 class Solution {
 public List<Integer> majorityElement(int[] nums) {
-    List<Integer> list = new ArrayList<Integer>();
-    int k = nums.length / 3 + 1, i;
-    HashMap<Integer, Integer> map = new HashMap<>();
-    for(int num : nums){
-        i = map.containsKey(num)? map.get(num) + 1 : 1;
-        map.put(num, i);
-        if(i == k)
-            list.add(num);
+    int e1=-1;
+    int e2=-3;
+    int c1=0;
+    int c2=0;
+    int n=nums.length;
+    for(int i=0;i<n;i++){
+        if(c1==0 && nums[i]!=e2){
+            c1=1;
+            e1=nums[i];
+        }
+        else if(c2==0 && nums[i]!=e1){
+            c2=1;
+            e2=nums[i];
+        }
+        else if(nums[i]==e1){
+            c1++;
+        }else if(nums[i]==e2){
+            c2++;
+        }else{
+            c1--;
+            c2--;
+        }
     }
-    return list;
+    c1=0;
+    c2=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]==e1) c1++;
+        if(nums[i]==e2) c2++;
+    }
+    int min=(int)n/3;
+    ArrayList<Integer> ans=new ArrayList();
+    if(c1>min) ans.add(e1);
+    if(c2>min) ans.add(e2);
+    return ans;
+
 }
 }
