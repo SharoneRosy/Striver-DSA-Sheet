b4d955aca88346a7e321401f63623e6bef864d57
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-03-25 17:43:53 +0530

    Time: 1 ms (95.57%) | Memory: 44.5 MB (45.87%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,27 @@
 class Solution {
-    public void solve(int [] arr, List<List<Integer>>ans, List<Integer>ds,boolean [] map){
-        if(ds.size()==arr.length){
-            ans.add(new ArrayList<>(ds));
-            return ;
-        }
-        for(int i=0;i<arr.length;i++){
-            if(map[i]!=true){
-                map[i]=true;
+    public void solve(int ind,int [] arr, List<List<Integer>>ans){
+        if(ind==arr.length){
+            List<Integer>ds=new ArrayList<>();
+            for(int i=0;i<arr.length;i++){
                 ds.add(arr[i]);
-                solve(arr,ans,ds,map);
-                ds.remove(ds.size()-1);
-                map[i]=false;
             }
+            ans.add(new ArrayList<>(ds));
         }
+        for(int i=ind;i<arr.length;i++){
+            swap(i,ind,arr);
+            solve(ind+1,arr,ans);
+            swap(i,ind,arr);
+        }
+
+    }
+    public void swap(int i,int j,int [] nums){
+       int temp=nums[i];
+       nums[i]=nums[j];
+       nums[j]=temp;
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>ds=new ArrayList<>();
-        boolean [] map=new boolean[nums.length];
-        solve(nums,ans,ds,map);
+        List<List<Integer>>ans=new ArrayList<>(); 
+        solve(0,nums,ans);
         return ans;
     }
 }
