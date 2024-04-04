e679e74387fc4bbbd058fe5b74e71961537bdc9a
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-04-04 20:47:46 +0530

    Time: 17 ms (68.00%) | Memory: 43.7 MB (70.68%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,32 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
-        int n=nums.length;
+        List<List<Integer>>ans=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1])  continue;
-            for(int j=i+1;j<n;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<nums.length;j++){
+                if(j>i+1 && nums[j]==nums[j-1])continue;
                 int k=j+1;
-                int l=n-1;
+                int l=nums.length-1;
                 while(k<l){
                     long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum<target){
                         k++;
-                    }
-                    if(sum>target){
+                    }else if(sum>target){
                         l--;
-                    }
-                    if(sum==target){
+                    }else{
                         ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        while(k<l && nums[k]==nums[k+1])k++;
+                        while(k<l && nums[l]==nums[l-1])l--;
                         k++;
                         l--;
-                        while(k<l && nums[k]==nums[k-1]) k++;
-                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
                 }
             }
+            
         }
         return ans;
+        
     }
 }
