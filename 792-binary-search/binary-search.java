40add537f65dc298e094edbe5e739feddc4daa1a
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-04-05 20:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (84.18%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -5,12 +5,12 @@ class Solution {
         int high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(target<nums[mid]){
-                high=mid-1;
-            }else if(target>nums[mid]){
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
                 low=mid+1;
             }else{
-                return mid;
+                high=mid-1;
             }
         }
         return -1;
