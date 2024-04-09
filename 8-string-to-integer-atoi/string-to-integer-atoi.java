cfcca21231e5db7d977f664efba39feb57df2824
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-04-09 19:53:17 +0530

    Time: 2 ms (36.99%) | Memory: 42 MB (86.09%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,26 +1,25 @@
 class Solution {
     public int myAtoi(String s) {
         s=s.trim();
-        if(s.isEmpty()){
+        if(s.isEmpty()) {
             return 0;
         }
-
-        int ans=0,i=0;
+        int ans=0;
+        int i=0;
+    
         boolean neg=s.charAt(0)=='-';
         boolean pos=s.charAt(0)=='+';
         if(neg || pos){
             i++;
         }
-        while(i<s.length() && Character.isDigit(s.charAt(i))){
-            int digit=s.charAt(i)-'0';
-            if(ans>Integer.MAX_VALUE/10|| (ans==Integer.MAX_VALUE/10 && digit>Integer.MAX_VALUE%10)){
+        while(i<s.length()   && Character.isDigit(s.charAt(i))){
+            int dig=s.charAt(i)-'0';
+            if(ans>Integer.MAX_VALUE/10 || (ans==Integer.MAX_VALUE/10 && dig>Integer.MAX_VALUE%10)){
                 return neg?Integer.MIN_VALUE:Integer.MAX_VALUE;
             }
-
-            ans=ans*10+digit;
+            ans=ans*10+dig;
             i++;
         }
-    
         return neg?-ans:ans;
     }
-    }
+}
