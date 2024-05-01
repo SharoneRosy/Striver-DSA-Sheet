121d14fc5ab31aaf36fac19b496d75599a7b272d
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-05-01 12:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (72.79%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,18 +1,20 @@
 class Solution {
-     public static int totalPaths(int row, int col, int[][] obstacleGrid, int[][] dp){
-           if(row < 0 || col < 0 || obstacleGrid[row][col] == 1) return 0;
-           if(row == 0 && col == 0) return 1;
-           if(dp[row][col] != -1) return dp[row][col];
-   
-           int up = totalPaths(row - 1, col, obstacleGrid, dp);
-           int left = totalPaths(row, col - 1, obstacleGrid, dp);
-           return dp[row][col] = up + left;
-       }
-       public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+   public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
            int n = obstacleGrid.length;
            int m = obstacleGrid[0].length;
            int[][] dp = new int[n][m];
-           for(int[] it : dp) Arrays.fill(it, -1);
-           return totalPaths(n - 1, m - 1, obstacleGrid, dp);
-       }
+           for(int row = 0; row < n; row++){
+               for(int col = 0; col < m; col++){
+                   if(obstacleGrid[row][col] == 1) dp[row][col] = 0;
+                   else if(row == 0 && col == 0) dp[row][col] = 1;
+                   else{
+                       int up = 0, left = 0;
+                       if(row > 0) up = dp[row - 1][col];
+                       if(col > 0) left = dp[row][col - 1];
+                       dp[row][col] = up + left;
+                   }
+               }
+           }
+           return dp[n - 1][m - 1];
+   }
 }
