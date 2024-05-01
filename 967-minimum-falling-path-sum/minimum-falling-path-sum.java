8205f7d54af21bab926099f4a915dfc1c5ce32b3
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-05-01 16:57:24 +0530

    Time: 8 ms (34.96%) | Memory: 45.2 MB (58.77%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,23 +1,23 @@
 class Solution {
     public static int minFallingPathSum(int[][] matrix) {
-           int n = matrix.length;
-           int[][] dp = new int[n][n];
-           for(int i = 0; i < n; i++){
-               for(int j = 0; j < n; j++){
-                   if(i == 0) dp[i][j] = matrix[i][j];
-                   else{
-                       int opt1 = Integer.MAX_VALUE, opt2 = Integer.MAX_VALUE, opt3 = Integer.MAX_VALUE;
-                       if(i - 1 >= 0 && j - 1 >= 0) opt1 = dp[i - 1][j - 1];
-                       if(i - 1 >= 0) opt2 = dp[i - 1][j];
-                       if(i - 1 >= 0 && j + 1 < n) opt3 = dp[i - 1][j + 1];
-               
-                       dp[i][j] = matrix[i][j] + Math.min(opt1, Math.min(opt2, opt3));
-                   }
-               }
+
+           int n=matrix.length;
+           int [][]dp=new int[n][n];
+           for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0) dp[i][j]=matrix[i][j];
+                else{
+                    int s=Integer.MAX_VALUE,ld=Integer.MAX_VALUE,rd=Integer.MAX_VALUE;
+                    if(i-1>=0 ) s=dp[i-1][j];
+                    if(i-1>=0 && j-1>=0) ld=dp[i-1][j-1];
+                    if(i-1>=0 && j+1<n) rd=dp[i-1][j+1];
+                    dp[i][j]=matrix[i][j]+Math.min(s,Math.min(ld,rd));
+                }
+            }
            }
-           int min = Integer.MAX_VALUE;
-           for(int j = 0; j < n; j++){
-               min = Math.min(min, dp[n - 1][j]);
+           int min=Integer.MAX_VALUE;
+           for(int j=0;j<n;j++){
+            min=Math.min(min,dp[n-1][j]);
            }
            return min;
        }
