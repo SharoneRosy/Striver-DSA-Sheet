3e35d9c7acec4ba922a51ad22f7a1e1aa3f626ad
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-05-06 13:52:23 +0530

    Time: 7 ms (83.01%) | Memory: 44.6 MB (61.37%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+     public static int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length(), m = text2.length();
+
+        // shifting 1-index to the right        
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    curr[j] = 1 + prev[j - 1];
+                }
+                else curr[j] = Math.max(prev[j], curr[j - 1]);
+            }
+            prev = curr.clone();
+        }
+
+        return prev[m];
+    }
+    public int minDistance(String word1, String word2) {
+        return word1.length()+word2.length()-2*(longestCommonSubsequence(word1,word2));
+    }
+}
