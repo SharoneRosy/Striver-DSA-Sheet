9973b04d3343e296924788d39470e7c13adf696b
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-07-15 16:41:48 +0530

    Time: 2 ms (99.88%) | Memory: 44.4 MB (89.86%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,32 +1,29 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s==null || t==null || s.length()==0 || t.length()==0 || s.length()<t.length()){
-            return new String();
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) {
+            map[c] += 1;
         }
-
-        int count=t.length();
-        int []cnt = new int[128];
-        for(char ch:t.toCharArray()){
-            cnt[ch]++;
-        }
-        char []ch = s.toCharArray();
-
-        int strt=0, end=0, min=Integer.MAX_VALUE, idx=0;
-        while(end < ch.length){
-            if(cnt[ch[end++]]-- > 0){
-                count--;
-            }
-            while(count==0){
-                if(end-strt < min){
-                    idx = strt;
-                    min = end-strt;
-                }
-                if(cnt[ch[strt++]]++ == 0){
+        int begin = 0;
+        int len = Integer.MAX_VALUE;
+        int count = t.length();
+        for (int left=0, right=0; right<s.length(); right++) {
+            char c = s.charAt(right);
+            map[c]--;
+            if(map[c]>=0) count--; 
+            while (count == 0) {
+                char lc = s.charAt(left);
+                map[lc]++;
+                if (map[lc]>0) {
+                    if (right-left+1<len) {
+                        begin = left;
+                        len = right-left+1;
+                    }
                     count++;
                 }
+                left++;
             }
         }
-
-        return min == Integer.MAX_VALUE?new String():new String(ch, idx, min);
+        return len==Integer.MAX_VALUE?"":s.substring(begin, begin+len);
     }
 }
