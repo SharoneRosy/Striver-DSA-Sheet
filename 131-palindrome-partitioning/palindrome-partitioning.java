15379cbc71fdfba2cb85f3c7dcb0886dc9ad2c96
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-07-16 16:58:40 +0530

    Time: 8 ms (49.22%) | Memory: 57.1 MB (16.10%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,29 +1,30 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> list = new ArrayList<>();
-        List<String> path = new ArrayList<>();
-        func(0,s,path,list);
-        return list;
-    }
-    static void func(int index,String s,List<String> path,List<List<String>> list){
-        if(index == s.length()){
-            list.add(new ArrayList<>(path));
+    public void solve(String s, int ind,List<String>list,List<List<String>>ans){
+        if(ind==s.length()){
+            ans.add(new ArrayList<>(list));
             return;
         }
-        for(int i = index;i<s.length();i++){
-            if(isPalindrom(s,index,i)){
-                path.add(s.substring(index,i+1));
-                func(i+1,s,path,list);
-                path.remove(path.size()-1);
-            }
+        for(int i=ind;i<s.length();i++){
+            if(isPalli(s,ind,i)){
+                 list.add(s.substring(ind,i+1));
+                solve(s,i+1,list,ans);
+                list.remove(list.size()-1);
+            }         
         }
     }
-    static boolean isPalindrom(String s,int start,int end){
-        while(start<=end){
-            if(s.charAt(start++)!=s.charAt(end--)){
-                return false;
-            }
+    public boolean isPalli(String s,int start,int end){
+        if(start>end){
+            return true;
         }
-        return true;
+        if(s.charAt(start)!=s.charAt(end)){
+            return false;
+        }
+       return isPalli(s,start+1,end-1);
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>>ans=new ArrayList<>();
+        List<String>list=new ArrayList<>();
+        solve(s,0,list,ans);
+        return ans;
     }
 }
