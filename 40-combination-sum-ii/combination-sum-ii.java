0bf8e9cf39f1258a27c375c49836e5dd72ba636f
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-07-22 18:56:31 +0530

    Time: 2 ms (99.58%) | Memory: 42.9 MB (80.06%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,5 +1,6 @@
 class Solution {
-    public void solve(int ind,int [] arr,int target,List<Integer>list,List<List<Integer>>ans){
+ 
+    public void solve(int ind,int [] arr, int target,List<List<Integer>>ans,List<Integer>list){
         if(target==0){
             ans.add(new ArrayList<>(list));
             return;
@@ -8,16 +9,16 @@ class Solution {
             if(i>ind && arr[i]==arr[i-1]) continue;
             if(arr[i]>target) break;
             list.add(arr[i]);
-            solve(i+1,arr,target-arr[i],list,ans);
+            solve(i+1,arr,target-arr[i],ans,list);
             list.remove(list.size()-1);
         }
-       
     }
     public List<List<Integer>> combinationSum2(int[] arr, int target) {
+       
         List<List<Integer>>ans=new ArrayList<>();
         List<Integer>list=new ArrayList<>();
         Arrays.sort(arr);
-        solve(0,arr,target,list,ans);
+        solve(0,arr,target,ans,list);
         return ans;
     }
 }
