d29337e7cb9489057186c4f1599758be8afa4608
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-07-27 16:55:31 +0530

    Time: 4 ms (47.95%) | Memory: 42.5 MB (29.38%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -42,55 +42,44 @@ class Pair{
 
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-
-        Queue<Pair> q = new LinkedList<>();
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-        q.offer(new Pair(root, 0, 0));
-
+        Queue<Pair>q=new LinkedList<>();
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>>map=new TreeMap<>();
+        q.add(new Pair(root,0,0));
         while(!q.isEmpty()){
-
-            for(int i = q.size() ; i > 0 ; i--){
-
-               Pair p = q.poll();
-               TreeNode currNode = p.node;
-               int x = p.row;
-               int y = p.col;
-
-               if(!map.containsKey(x))
-                    map.put(x, new TreeMap<>());
-
-                if(!map.get(x).containsKey(y))
-                    map.get(x).put(y, new PriorityQueue<>());
+            for(int i=q.size();i>0;i--){
+                Pair p=q.poll();
+                TreeNode currNode=p.node;
+                int x=p.row;
+                int y=p.col;
+                if(!map.containsKey(x)){
+                    map.put(x,new TreeMap<>());
+                }
+                if(!map.get(x).containsKey(y)){
+                    map.get(x).put(y,new PriorityQueue<>());
+                }
 
                 map.get(x).get(y).offer(currNode.val);
+                
+                if(currNode.left!=null){
+                    q.offer(new Pair(currNode.left,x-1,y+1));
+                }
 
-               if(currNode.left != null){
-                   q.offer(new Pair(currNode.left, x - 1, y + 1));
-               }
-
-               if(currNode.right != null){
-                   q.offer(new Pair(currNode.right, x + 1, y + 1));
-               }
+                if(currNode.right!=null){
+                    q.offer(new Pair(currNode.right,x+1,y+1));
+                }
             }
         }
-
-
-
-        List<List<Integer>> ans = new ArrayList<>();
-
-        for(TreeMap<Integer, PriorityQueue<Integer>> key : map.values()){
-            List<Integer> currList = new ArrayList<>();
-
-            for(PriorityQueue<Integer> nodes : key.values()){
+       
+        List<List<Integer>>ans=new ArrayList<>();
+        for(TreeMap<Integer,PriorityQueue<Integer>>key:map.values()){
+            List<Integer>currList=new ArrayList<>();
+            for(PriorityQueue<Integer>nodes:key.values()){
                 while(!nodes.isEmpty()){
                     currList.add(nodes.poll());
                 }
             }
-           
             ans.add(new ArrayList<>(currList));
-
         }
-    
         return ans;
         
     }
