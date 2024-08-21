dd82003816fd07fb86cc45c9d98d5379e1cd81ba
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-08-21 17:03:24 +0530

    Time: 6 ms (45.03%) | Memory: 45 MB (44.56%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,29 +1,31 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-       List<List<Integer>>adjList=new ArrayList<>();
+       
+
+       List<List<Integer>>adj=new ArrayList<>();
        for(int i=0;i<numCourses;i++){
-        adjList.add(new ArrayList<>());
+        adj.add(new ArrayList<>());
        }
        int [] indegree=new int[numCourses];
-       for(int []prerequisite: prerequisites){
-            adjList.get(prerequisite[1]).add(prerequisite[0]);
-            indegree[prerequisite[0]]++;
+       for(int []row:prerequisites){
+        adj.get(row[1]).add(row[0]);
+        indegree[row[0]]++;
        }
-
        Queue<Integer>q=new LinkedList<>();
        for(int i=0;i<numCourses;i++){
         if(indegree[i]==0){
             q.add(i);
         }
        }
+
        Set<Integer>vis=new HashSet<>();
        while(!q.isEmpty()){
         int course=q.poll();
         vis.add(course);
-        for(int neg:adjList.get(course)){
-            indegree[neg]--;
-            if(indegree[neg]==0){
-                q.add(neg);
+        for(int adjnode:adj.get(course)){
+            indegree[adjnode]--;
+            if(indegree[adjnode]==0){
+                q.add(adjnode);
             }
         }
        }
