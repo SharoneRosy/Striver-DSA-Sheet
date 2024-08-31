0f4a3a21cd342d510c474957fffc5f8785383b77
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-08-31 18:13:25 +0530

    Time: 17 ms (73.64%) | Memory: 48.5 MB (21.19%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,31 +1,33 @@
-class Solution {
-    public static boolean isAllStars(String p, int idx){
-           while(idx >= 0) {
-               if(p.charAt(idx--) != '*') return false;
-           } 
-           return true;
-       }
-   
-       public static boolean helper(int i, int j, String s, String p, Boolean[][] dp){
-           if(i < 0 && j < 0) return true;
-           else if(i < 0 && j >= 0) return isAllStars(p, j);
-           else if(j < 0 && i >= 0) return false;
-   
-           if(dp[i][j] != null) return dp[i][j];
-   
-           if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
-               return dp[i][j] = helper(i - 1, j - 1, s, p, dp);
-           }
-           else if(p.charAt(j) == '*'){
-               return dp[i][j] = helper(i, j - 1, s, p, dp) || helper(i - 1, j, s, p, dp);
-           }    
-           return dp[i][j] = false;    
-       }
-   
-       public static boolean isMatch(String s, String p) {
-           int n = s.length();
-           int m = p.length();
-           Boolean[][] dp = new Boolean[n][m];
-           return helper(n - 1, m - 1, s, p, dp);
-       }
+ class Solution {
+    private int memoization(String s, String p, int i, int j, int[][] dp){
+        if(i<0 && j<0) return 1;
+        if(i<0 && j>=0){
+             for(int x=0; x<=j; x++){
+                if(p.charAt(x) != '*') return 0;
+            }
+            return 1;
+        };
+        if(j<0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+            return dp[i][j] = memoization(s, p ,i-1, j-1, dp);
+        }
+        if(p.charAt(j) == '*'){
+            return dp[i][j] = memoization(s, p, i-1, j, dp) == 1 || memoization(s, p, i, j-1, dp) == 1 ? 1 : 0;
+        }
+
+        return dp[i][j] = 0;
+    }
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        int[][] dp = new int[n][m];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+
+        return memoization(s, p, n-1, m-1, dp) == 1 ? true: false;
+    }
 }
