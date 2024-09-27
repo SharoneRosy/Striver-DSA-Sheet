2dee7308081d751c6d8f55f8507319f76931e9a9
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-09-27 06:12:41 -0700

    Time: 7 ms (98.06%) | Memory: 56.8 MB (67.69%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,25 +1,21 @@
 class Solution {
-    public void solve(String s, int ind,List<String>list,List<List<String>>ans){
+    public void solve(String s,int ind,List<String>list,List<List<String>>ans){
         if(ind==s.length()){
             ans.add(new ArrayList<>(list));
-            return;
+            return ;
         }
         for(int i=ind;i<s.length();i++){
             if(isPalli(s,ind,i)){
-                 list.add(s.substring(ind,i+1));
+                list.add(s.substring(ind,i+1));
                 solve(s,i+1,list,ans);
                 list.remove(list.size()-1);
-            }         
+            }
         }
     }
-    public boolean isPalli(String s,int start,int end){
-        if(start>end){
-            return true;
-        }
-        if(s.charAt(start)!=s.charAt(end)){
-            return false;
-        }
-       return isPalli(s,start+1,end-1);
+    public boolean isPalli(String s, int start,int end){
+        if(start>end)return true;
+        if(s.charAt(start)!=s.charAt(end)) return false;
+        return isPalli(s,start+1,end-1);
     }
     public List<List<String>> partition(String s) {
         List<List<String>>ans=new ArrayList<>();
