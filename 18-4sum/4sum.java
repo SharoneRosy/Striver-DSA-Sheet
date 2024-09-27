610f80dbcd5347deea80746e12d1419e2bf07c55
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-09-26 19:58:24 -0700

    Time: 16 ms (81.53%) | Memory: 43.6 MB (84.70%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,13 +1,43 @@
+// class Solution {
+//     public List<List<Integer>> fourSum(int[] nums, int target) {
+//         List<List<Integer>>ans=new ArrayList<>();
+//         Arrays.sort(nums);
+//         int n=nums.length;
+//         for(int i=0;i<n;i++){
+//             if(i>0 && nums[i]==nums[i-1]) continue;
+//             for(int j=i+1;j<n;j++){
+//                 if(j>i+1 && nums[j]==nums[j-1]) continue;
+//                 int k=j+1,l=n-1;
+//                 while(k<l){
+//                     long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+//                    if(sum<target){
+//                         k++;
+//                     }else if(sum >target){
+//                         l--;
+//                     }else {
+//                         ans.add(Arrays.asList(nums[i],nums[j]+nums[k]+nums[l]));
+//                         while(k<l && nums[k]==nums[k+1])k++;
+//                         while(k<l && nums[l]==nums[l-1])l--;
+//                         k++;
+//                         l--;
+//                     } 
+//                 }
+//             }
+//         }
+//          return ans;
+//     }
+// }
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>>ans=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
+        int n=nums.length;
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<nums.length;j++){
-                if(j>i+1 && nums[j]==nums[j-1])continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
                 int k=j+1;
-                int l=nums.length-1;
+                int l=n-1;
                 while(k<l){
                     long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum<target){
@@ -16,16 +46,14 @@ class Solution {
                         l--;
                     }else{
                         ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                        while(k<l && nums[k]==nums[k+1])k++;
-                        while(k<l && nums[l]==nums[l-1])l--;
+                        while(k<l && nums[k]==nums[k+1]) k++;
+                        while(k<l && nums[l]==nums[l-1]) l--;
                         k++;
-                        l--;
+                        l--; 
                     }
                 }
             }
-            
         }
         return ans;
-        
     }
 }
