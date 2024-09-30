de81dea3e7a8204f19730df4b4597a5a4d4cca36
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-09-30 20:52:30 +0530

    Time: 5 ms (77.13%) | Memory: 45.3 MB (68.50%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,38 +1,17 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        if(timePoints.size() > 1440) return 0;
-        int minutes[] = new int[1440];
-
-        int max_minutes = 0;
-
-        for(int i = 0; i < timePoints.size(); i++) {
-            String str = timePoints.get(i);
-            int h = (str.charAt(0) - 48) * 10 + (str.charAt(1) - 48);
-            int m = (str.charAt(3) - 48) * 10 + (str.charAt(4) - 48);
-            int mins = h * 60 + m;
-
-            if(mins > max_minutes) max_minutes = mins;
-            if(minutes[mins] == 1) return 0;
-            minutes[mins] = 1;
+        int []min=new int[timePoints.size()];
+        for(int i=0;i<timePoints.size();i++){
+            String time=timePoints.get(i);
+            int h=Integer.parseInt(time.substring(0,2));
+            int m=Integer.parseInt(time.substring(3));
+            min[i]=h*60+m;
         }
-
-        int result = 1450;
-        int first = -1;
-        int prev = -1;
-        for(int i = 0; i < 1440; i++) {
-            if(minutes[i] == 1) {
-                if(first == -1) {
-                    first = i;
-                    prev = i;
-                }
-                else {
-                    result = Math.min(result, i - prev);
-                    prev = i;
-                }
-            }
+        Arrays.sort(min);
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<min.length-1;i++){
+            ans=Math.min(ans,min[i+1]-min[i]);
         }
-        result = Math.min(result, 1440 + first - max_minutes);
-        return result;
-
+        return Math.min(ans,24*60-min[min.length-1]+min[0]);
     }
 }
