f2f46289ac5ba4adaf084751aa615f418d7df098
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-09-30 18:25:26 +0530

    Time: 2 ms (99.92%) | Memory: 41.9 MB (77.15%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public String[] uncommonFromSentences(String A, String B) {
+      Map<String,Integer>count=new HashMap<>();
+      for(String word: A.split(" ")){
+        count.put(word,count.getOrDefault(word,0)+1);
+      }
+       for(String word: B.split(" ")){
+        count.put(word,count.getOrDefault(word,0)+1);
+      }
+      List<String>ans=new ArrayList<>();
+      for(String word: count.keySet()){
+        if(count.get(word)==1){
+            ans.add(word);
+        }
+      }
+      return ans.toArray(new String[ans.size()]);
+    }
+}
