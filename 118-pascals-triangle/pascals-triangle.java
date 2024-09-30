91d4d4d3fb7af5fd7d565be154c3690736b4c270
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-09-30 15:12:17 +0530

    Time: 1 ms (84.00%) | Memory: 41.7 MB (90.83%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,23 +1,18 @@
 class Solution {
-
-    public static int nCr(int n,int r){
-        int res=1;
-        for(int i=0;i<r;i++){
-            res=res*(n-i);
-            res=res/(i+1);
-        }
-        return res;
-    }
-    public List<List<Integer>> generate(int n){
-        List<List<Integer>> ans=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            List<Integer> row=new ArrayList<>();
-            for(int j=0;j<=i;j++){
-                row.add(nCr(i,j));
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>>tri=new ArrayList<>();
+        tri.add(new ArrayList<>());
+        tri.get(0).add(1);
+        for(int i=1;i<n;i++){
+            List<Integer>row=new ArrayList<>();
+            List<Integer>prow=tri.get(i-1);
+            row.add(1);
+            for(int j=1;j<i;j++){
+                row.add(prow.get(j-1)+prow.get(j));
             }
-            ans.add(row);
+            row.add(1);
+            tri.add(row);
         }
-        return ans;
+        return tri;
     }
-    
 }
