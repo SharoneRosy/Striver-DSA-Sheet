dc90122316a7f80908065d0753e41a18c4d56f68
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-01 11:24:10 +0530

    Time: 3 ms (19.80%) | Memory: 42.9 MB (30.11%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -2,36 +2,26 @@ class Solution {
     List<Integer> solve(String s)
     {
         List<Integer> result = new ArrayList<>();
-        for(int i=0; i<s.length(); i++)
-        {
+        for(int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch=='+'||ch=='-'||ch=='*')
-            {
+            if(ch=='+'||ch=='-'||ch=='*'){
                 List<Integer> left = new ArrayList<>();
                 left = solve(s.substring(0,i));
                 List<Integer> right = new ArrayList<>();
                 right = solve(s.substring(i+1));
 
-                for(int x: left)
-                {
-                    for(int y: right)
-                    {
-                        if(ch=='+')
-                        result.add(x+y);
-                        else if(ch=='-')
-                        result.add(x-y);
-                        else
-                        result.add(x*y);
+                for(int x: left){
+                    for(int y: right)  {
+                        if(ch=='+') result.add(x+y);
+                        else if(ch=='-') result.add(x-y);
+                        else result.add(x*y);
                     }
                 }
             }
         }
-        if(result.size()==0)
-        result.add(Integer.valueOf(s));
+        if(result.size()==0) result.add(Integer.valueOf(s));
         return result;
     }
-
-
     public List<Integer> diffWaysToCompute(String expression) {
         return solve(expression);
     }
