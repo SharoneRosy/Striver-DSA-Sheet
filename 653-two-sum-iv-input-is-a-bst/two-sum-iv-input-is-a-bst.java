a535090fd4192a699ba10ea1fe10d613f5127873
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-02 13:02:53 +0530

    Time: 3 ms (64.23%) | Memory: 45.1 MB (55.79%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,14 +1,60 @@
-public class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-        Set < Integer > set = new HashSet();
-        return find(root, k, set);
+class BSTIterator {
+Stack<TreeNode> s = new Stack<>();
+Boolean reverse = true;
+public BSTIterator(TreeNode root,Boolean reverse) {
+    this.reverse = reverse;
+    pushAll(root);
+}
+public int next() {
+    TreeNode temp = s.pop();
+    if(reverse == false){
+        pushAll(temp.right);
     }
-    public boolean find(TreeNode root, int k, Set < Integer > set) {
-        if (root == null)
-            return false;
-        if (set.contains(k - root.val))
-            return true;
-        set.add(root.val);
-        return find(root.left, k, set) || find(root.right, k, set);
+    else{
+        pushAll(temp.left);
+    }
+    return temp.val;
+    
+}
+
+//This function just tells whether there are any elements left or not.
+public boolean hasNext() {
+    return !s.isEmpty();
+}
+public void pushAll(TreeNode node){
+    
+    if(reverse == false){
+        while(node!=null){
+        s.push(node);
+        node = node.left;
     }
 }
+    else{
+        while(node!=null){
+        s.push(node);
+        node = node.right;
+		}
+	}
+}
+}
+class Solution {
+public boolean findTarget(TreeNode root, int k) {
+
+	//Creating an instance of the BSTIterator class to get the desired traversal.
+    BSTIterator left = new BSTIterator(root,false);
+    BSTIterator right = new BSTIterator(root,true);
+    
+	//Taking the first element of the ascending and descending traversals.
+    int i = left.next();
+    int j = right.next();
+    
+	//Using simple binary search logic to find the answer.
+    while(i<j){
+        if(i+j == k) return true;
+        else if(i+j< k) i = left.next();
+        else j =right.next();
+        }
+    return false;
+    }
+}
+
