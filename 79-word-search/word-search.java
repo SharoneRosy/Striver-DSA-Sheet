1d465a090deb26a72c426d75e90f6f10e17020e1
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-05 17:38:47 +0530

    Time: 522 ms (7.02%) | Memory: 40.9 MB (98.86%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,33 +1,29 @@
 class Solution {
-    boolean visited[][];
-    public boolean exist(char[][] board, String word) {
-        int rows=board.length;
-        int cols=board[0].length;
-        visited=new boolean[rows][cols];
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(word.charAt(0)==board[i][j] && solve(i,j,0,word,board)){
-                    return true;
-                }
+    int[] dr1 = {1, -1, 0, 0};
+    int[] dr2 = {0, 0, 1, -1};
+
+    public boolean solve(char[][] board, int row, int col, int i, int j, String word, int ind, int n) {
+        if (ind == n - 1 && word.charAt(ind) == board[i][j]) return true;
+        for (int d = 0; d < 4; d++) {
+            int x = i + dr1[d], y = j + dr2[d];
+            if (x >= 0 && x < row && y >= 0 && y < col && board[x][y] != '.' && board[x][y] == word.charAt(ind)) {
+                char ch = board[x][y];
+                board[x][y] = '.';
+                if (ind == n - 1) return true;
+                if (solve(board, row, col, x, y, word, ind + 1, n)) return true;
+                board[x][y] = ch;
             }
         }
         return false;
     }
-    public boolean solve(int i,int j,int curr,String word,char[][] board){
-        if(curr==word.length()){
-            return true;
-        }
-        if(i<0|| i>=board.length || j<0 || j>=board[i].length || board[i][j]!=word.charAt(curr) || visited[i][j]){
-            return false;
-        }
-        visited[i][j]=true;
-        if(solve(i+1,j,curr+1,word,board) ||
-            solve(i-1,j,curr+1,word,board) ||
-            solve(i,j-1,curr+1,word,board) ||
-            solve(i,j+1,curr+1,word,board)){
-                return true;
+
+    public boolean exist(char[][] board, String word) {
+        int row = board.length, col = board[0].length, n = word.length();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (solve(board, row, col, i, j, word, 0, n)) return true;
             }
-            visited[i][j]=false;
-            return false;
+        }
+        return false;
     }
 }
