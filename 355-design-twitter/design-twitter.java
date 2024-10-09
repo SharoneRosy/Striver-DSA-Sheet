bcd4aa86ecde1a8620ccf72a1e7293c804c6340c
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-09 10:07:03 +0530

    Time: 9 ms (56.73%) | Memory: 41.2 MB (95.50%) - LeetSync

--- 355-design-twitter/design-twitter.java
+++ 355-design-twitter/design-twitter.java
@@ -1,61 +1,43 @@
 class Twitter {
-
-    HashMap<Integer, Integer> postedTweets = new HashMap<>();
-    List<Integer> follower = new ArrayList<>();
-    List<Integer> followee = new ArrayList<>();
-    List<Integer> allTweets = new ArrayList<>();
+    private List<int[]> recentTweets;
+    private Map<Integer, Set<Integer>> follows;
 
     public Twitter() {
-        
+        recentTweets = new ArrayList<>();
+        follows = new HashMap<>();
     }
-    
+
     public void postTweet(int userId, int tweetId) {
-        //Since I have a unique tweetId, I can use it as key and the user that posted it as value
-        postedTweets.put(tweetId, userId);
-        allTweets.add(tweetId);
+        recentTweets.add(new int[]{userId, tweetId});
     }
-    
+
     public List<Integer> getNewsFeed(int userId) {
-        List<Integer> res = new ArrayList<>();
-        List<Integer> validFollowers = new ArrayList<>();
-        for(int i = 0; i < follower.size(); i++){
-            if(follower.get(i) == userId){
-                validFollowers.add(followee.get(i));
+        List<Integer> feed = new ArrayList<>();
+        Set<Integer> userFollows = follows.getOrDefault(userId, new HashSet<>());
+
+        for (int i = recentTweets.size() - 1; i >= 0; --i) {
+            int[] tweet = recentTweets.get(i);
+            int postedBy = tweet[0];
+            int tweetId = tweet[1];
+
+            if (postedBy == userId || userFollows.contains(postedBy)) {
+                feed.add(tweetId);
             }
+
+            if (feed.size() == 10) break;
         }
-        int counter = 0;
-        for(int i = allTweets.size() - 1; i >= 0; i--){
-            
-            int tweetPoster = postedTweets.get(allTweets.get(i));
-            if(tweetPoster == userId || validFollowers.contains(tweetPoster)){
-                if(counter < 10){
-                    res.add(allTweets.get(i));
-                    counter++;
-                }
-            }
-        }
-        return res;
+        
+        return feed;
     }
-    
+
     public void follow(int followerId, int followeeId) {
-        follower.add(followerId); followee.add(followeeId);
+        follows.putIfAbsent(followerId, new HashSet<>());
+        follows.get(followerId).add(followeeId);
     }
-    
+
     public void unfollow(int followerId, int followeeId) {
-        for(int i = 0; i < follower.size(); i++){
-            if(follower.get(i) == followerId && followee.get(i) == followeeId){
-                follower.remove(i); followee.remove(i);
-            }
+        if (follows.containsKey(followerId)) {
+            follows.get(followerId).remove(followeeId);
         }
     }
-}
-
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter obj = new Twitter();
- * obj.postTweet(userId,tweetId);
- * List<Integer> param_2 = obj.getNewsFeed(userId);
- * obj.follow(followerId,followeeId);
- * obj.unfollow(followerId,followeeId);
- */
+    }
