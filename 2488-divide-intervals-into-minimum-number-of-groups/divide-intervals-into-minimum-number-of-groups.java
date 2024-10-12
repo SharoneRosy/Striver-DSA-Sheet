96db5c51a277bbe164254dc4e730e29d1ec7cd7b
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-12 11:56:00 +0530

    Time: 98 ms (35.87%) | Memory: 75.7 MB (70.11%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
@@ -1,27 +1,13 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        int n=intervals.length;
-        int start[]=new int[n];
-        int end[]=new int[n];
-        for(int i=0;i<n;i++){
-            start[i]=intervals[i][0];
-            end[i]=intervals[i][1];
-        }
-        Arrays.sort(start);
-        Arrays.sort(end);
-        int cnt=0;
-        int max=Integer.MIN_VALUE;
-        int i=0,j=0;
-        while(i<n && j<n){
-            if(start[i]<=end[j]){
-                cnt++;
-                i++;
-            }else{
-                cnt--;
-                j++;
+        Arrays.sort(intervals,(a,b)->a[0]==b[0]? a[1]-b[1]: a[0]-b[0]);
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int [] i:intervals){
+            if(!pq.isEmpty() && i[0]>pq.peek()){
+                pq.poll();
             }
-           max=Math.max(cnt,max);
+            pq.add(i[1]);
         }
-        return max;
+        return pq.size();
     }
 }
