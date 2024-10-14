dd8833c64ae13651b657d01cf1193309b51e0157
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-14 09:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (80.85%) - LeetSync

--- 1345-perform-string-shifts/perform-string-shifts.java
+++ 1345-perform-string-shifts/perform-string-shifts.java
@@ -1,17 +1,25 @@
 class Solution {
   public String stringShift(String string, int[][] shift) {
-    int len = string.length();
+    
+    // Add up the left shifts and right shifts.
+    int[] overallShifts = new int[2];
     for (int[] move : shift) {
-      int direction = move[0];
-      int amount = move[1] % len;
-      if (direction == 0) {
-        // Move necessary amount of characters from front to end 
-        string = string.substring(amount) + string.substring(0, amount);
-      } else {
-        // Move necessary amount of characters from end to front
-        string = string.substring(len - amount) + string.substring(0, len - amount);
-      }
+      overallShifts[move[0]] += move[1];
     }
+    int leftShifts = overallShifts[0];
+    int rightShifts = overallShifts[1];
+      
+    // Determine which shift (if any) to perform.
+    int len = string.length();
+    if (leftShifts > rightShifts) {
+      leftShifts = (leftShifts - rightShifts) % len;
+      string = string.substring(leftShifts) + string.substring(0, leftShifts);
+    }
+    else if (rightShifts > leftShifts) {
+      rightShifts = (rightShifts - leftShifts) % len;
+      string = string.substring(len - rightShifts) + string.substring(0, len - rightShifts);
+    }
+    
     return string;
   }
 }
