d0a4b2e07a218e2cb24fb5e8e7dca1fb53d91c41
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-21 16:56:18 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (37.86%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,58 +1,19 @@
 class Solution {
     public int myAtoi(String s) {
-        
-
-        // s=s.trim();
-        // if(s.isEmpty()) return 0;
-        // int ans=0;
-        // int i=0;
-        //  int max=Integer.MAX_VALUE;
-        //  int min=Integer.MIN_VALUE;
-
-        // boolean neg=s.charAt(0)=='-';
-        // boolean pos=s.charAt(0)=='+';
-        // if(neg|| pos){
-        //     i++;
-        // }
-        // while(i<s.length() &&  Character.isDigit(s.charAt(i))){
-        //     int digit = s.charAt(i)-'0';
-        //         if(ans>max/10 || (ans==max/10 && digit>max%10)){
-        //          return neg ? max : min;            
-        //      }
-        //     ans=ans*10+digit;
-        //     i++;
-        // }
-        // return neg?-ans:ans;
-
-         s = s.strip();
-        if (s.isEmpty()) return 0;
-        final int sign = s.charAt(0) == '-' ? -1 : 1;
-        
-        if (s.charAt(0) == '+' || s.charAt(0) == '-')
-            s = s.substring(1);
-        int startIdx = 0;
-        int endIdx = 0;
-        long ans = 0;
-        ans = helper(s,startIdx,endIdx, ans, sign);
-        System.out.println(sign + " " + ans);
-        if (ans <= Integer.MIN_VALUE) return sign * (int) ans ;
-        if (ans >= Integer.MAX_VALUE) return sign * (int) ans ; 
-        return sign * (int)ans; 
-    }
-     public long helper(String in,int startIdx, int endIdx, long ans, int sign){
-        if(endIdx == in.length() || in.charAt(endIdx) - '0' > 9) return ans;
-
-        int startDig = in.charAt(startIdx) - '0';
-        if(startDig < 0 && startDig > 9) return helper(in, startIdx++, endIdx++, ans, sign);
-
-        int endDig = in.charAt(endIdx) - '0';
-        if(endDig >= 0 && endDig <= 9) {
-            ans = ans * 10;
-            ans += endDig;
-            if (sign * ans <= Integer.MIN_VALUE) return Integer.MIN_VALUE ;
-            if (sign * ans >= Integer.MAX_VALUE) return Integer.MAX_VALUE ;
-            return helper(in, startIdx, ++endIdx, ans, sign);
+        s=s.trim();
+        if(s.isEmpty()) return 0;
+        int ans=0,i=0;
+        boolean neg=s.charAt(0)=='-';
+        boolean pos=s.charAt(0)=='+';
+        if(neg|| pos) i++;
+        while( i<s.length() && Character.isDigit(s.charAt(i))){
+            int digit=s.charAt(i)-'0';
+            if(ans>Integer.MAX_VALUE/10 || (ans==Integer.MAX_VALUE/10 && digit>Integer.MAX_VALUE%10)){
+                return neg?Integer.MIN_VALUE:Integer.MAX_VALUE;
+            }
+            ans=ans*10+digit;
+            i++;
         }
-        return ans;
+        return neg?-ans:ans;
     }
 }
