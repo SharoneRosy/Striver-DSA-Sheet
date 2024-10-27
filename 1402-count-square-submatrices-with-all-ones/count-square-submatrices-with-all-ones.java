14c868d8c97d453d14bed6e89bb28b8aca45ca6e
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-27 13:15:01 +0530

    Time: 5 ms (98.78%) | Memory: 54.7 MB (85.04%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,33 +1,22 @@
 class Solution {
-    private int sum(int [][]dp){
-        int sum=0;
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
-                sum += dp[i][j];
-            }
-        }
-        return sum;
-    }
-
     public int countSquares(int[][] matrix) {
-        int [][]dp = new int[matrix.length][matrix[0].length];
-        for(int j=0; j<matrix[0].length; j++){
-            dp[0][j] = matrix[0][j];
-        }
-        for(int i=0; i<matrix.length; i++){
-            dp[i][0] = matrix[i][0];
-        }
-
-        for(int i=1; i<matrix.length; i++){
-            for(int j=1; j<matrix[0].length; j++){
-                if(matrix[i][j]==0){
-                    dp[i][j]=0;
-                }
-                else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
-                } 
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int [][]dp=new int[m][n];
+        for(int i=0;i<n;i++) dp[0][i]=matrix[0][i];
+        for(int i=0;i<m;i++) dp[i][0]=matrix[i][0];
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0) continue;
+                dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
             }
         }
-        return sum(dp);
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cnt+=dp[i][j];
+            }
+        }
+        return cnt;
     }
 }
