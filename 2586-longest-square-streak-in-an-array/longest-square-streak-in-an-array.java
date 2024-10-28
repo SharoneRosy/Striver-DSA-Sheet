ffcad0767d8c98a3c12cdcc6ff47956b69143ccc
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-10-28 10:18:10 +0530

    Time: 41 ms (78.13%) | Memory: 62.1 MB (20.14%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
@@ -1,19 +1,31 @@
 class Solution {
     public int longestSquareStreak(int[] nums) {
-        int maxLength=-1;
-        HashSet<Long>set=new HashSet<>();
-        for(int i:nums) set.add((long)i);
-        for(int i:nums){
-            int cnt=1;
-            long curr=i;
-            while(curr<100000 && set.contains(curr*curr)){
-                cnt++;
-                curr=curr*curr;
+        // Space: O(N) for HashSet
+        HashSet<Long> set = new HashSet<>();
+        
+        // Time: O(N) for initial HashSet population
+        for (int num : nums) {
+            set.add((long)num);
+        }
+        
+        int maxLength = -1;
+        
+        // Time: O(N) * O(loglogM) for checking each number's streak
+        for (int num : nums) {
+            int currentLength = 1;
+            long current = num;
+            
+            // This while loop runs O(loglogM) times because of how quickly squares grow
+            while (current <= 100000 && set.contains(current * current)) {
+                currentLength++;
+                current = current * current;
             }
-            if(cnt>=2){
-                maxLength=Math.max(maxLength,cnt);
+            
+            if (currentLength >= 2) {
+                maxLength = Math.max(maxLength, currentLength);
             }
         }
+        
         return maxLength;
     }
 }
