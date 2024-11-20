5cdb67d3b8465233234ee2f714d5d7160b9e59a0
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-11-20 12:15:49 +0530

    Time: 13 ms (74.55%) | Memory: 44.5 MB (100.00%) - LeetSync

---
+++ 2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int takeCharacters(String s, int k) {
+        int n=s.length();
+        int [] arr=new int[3];
+        for(char ch:s.toCharArray()) arr[ch-'a']++;
+
+        if(arr[0]<k || arr[1]<k || arr[2]<k) return -1;
+        
+        int left=0,right=0;
+        int minwindow=n;
+
+        while( right<n){
+            arr[s.charAt(right)-'a']--;
+            while(left<=right && arr[0]<k || arr[1]<k || arr[2]<k){
+                arr[s.charAt(left)-'a']++;
+                left++;
+            }
+            minwindow=Math.min(minwindow,n-(right-left+1));
+            right++;
+        }
+        return minwindow;
+    }
+}
