52c15131df10644d59f891146e7460a119c14695
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-12-16 14:46:01 +0530

    Time: 3 ms (33.80%) | Memory: 44.3 MB (97.81%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,14 +1,20 @@
-
-import java.util.stream.IntStream;
 class Solution {
-    
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        for(int i=0;i<k;i++){
-            // int minIndex=IntStream.range(0,nums.length).reduce((a,b)->nums[a]<=nums[b]:a?b).orElse(-1);
-            int minIndex = IntStream.range(0, nums.length)
-                    .reduce((a, b) -> nums[a] <= nums[b] ? a : b)
-                    .orElse(-1);
-            nums[minIndex]*=multiplier;
+        PriorityQueue<int []>minHeap=new PriorityQueue<>((a,b)->
+        {
+            if(a[0]==b[0]){
+                return Integer.compare(a[1],b[1]);
+            }
+            return Integer.compare(a[0],b[0]);
+        });
+        for(int i=0;i<nums.length;i++){
+            minHeap.add(new int[]{nums[i],i});
+        }
+        while(k-->0){
+            int[] curr=minHeap.poll();
+            curr[0]*=multiplier;
+            nums[curr[1]]*=multiplier;
+            minHeap.offer(curr);
         }
         return nums;
     }
