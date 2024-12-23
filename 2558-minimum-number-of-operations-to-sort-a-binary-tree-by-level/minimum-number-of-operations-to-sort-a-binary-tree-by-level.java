419822cb1028c133180cfedb5d2183470eae6187
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2024-12-23 10:33:24 +0530

    Time: 76 ms (37.67%) | Memory: 66.7 MB (58.90%) - LeetSync

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -14,11 +14,29 @@
  * }
  */
 class Solution {
+    public int minSwaps(List<Integer> level){
+        int swaps=0;
+        List<Integer>sort_level=new ArrayList<>(level);
+        Collections.sort(sort_level);
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<level.size();i++){
+            map.put(level.get(i),i);
+        }
+        for(int i=0;i<level.size();i++){
+            while(!level.get(i).equals(sort_level.get(i))){
+                swaps++;
+                int curr=map.get(sort_level.get(i));
+                map.put(level.get(i),curr);
+                Collections.swap(level,i,curr);
+            }
+        }
+        return swaps;
+    }
     public int minimumOperations(TreeNode root) {
         if(root==null) return 0;
         Queue<TreeNode>q=new LinkedList<>();
-        q.add(root);
         int operations=0;
+        q.add(root);
         while(!q.isEmpty()){
             int size=q.size();
             List<Integer>level=new ArrayList<>();
@@ -28,22 +46,7 @@ class Solution {
                 if(node.left!=null) q.add(node.left);
                 if(node.right!=null) q.add(node.right);
             }
-
-            List<Integer>sort_level=new ArrayList<>(level);
-            Collections.sort(sort_level);
-            Map<Integer,Integer>map=new HashMap<>();
-            for(int i=0;i<level.size();i++){
-                map.put(level.get(i),i);
-            }
-
-            for(int i=0;i<level.size();i++){
-                while(!level.get(i).equals(sort_level.get(i))){
-                    operations++;
-                    int cur=map.get(sort_level.get(i));
-                    map.put(level.get(i),cur);
-                    Collections.swap(level,i,cur);
-                }
-            }
+            operations+=minSwaps(level);
         }
         return operations;
     }
