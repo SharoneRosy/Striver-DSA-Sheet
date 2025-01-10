7b7164919411059f2df277aa61aeb16c2a8a8740
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-10 21:22:40 +0530

    Time: 4 ms (41.48%) | Memory: 43.1 MB (6.13%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,56 +1,62 @@
-class Pair {
-    TreeNode node;
-    int row, col;
-    
-    Pair(TreeNode node, int row, int col) {
-        this.node = node;
-        this.row = row; // Vertical position
-        this.col = col; // Horizontal position
-    }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    class Pair{
+        TreeNode node;
+        int row,col;
+        Pair(TreeNode node,int row,int col){
+            this.node=node;
+            this.row=row;
+            this.col=col;
+        }
+    }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        Queue<Pair> q = new LinkedList<>();
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-        
-        q.add(new Pair(root, 0, 0)); // Start with root at row=0, col=0
-        
-        while (!q.isEmpty()) {
-            for (int i = q.size(); i > 0; i--) {
-                Pair p = q.poll();
-                TreeNode currNode = p.node;
-                int y = p.col;  // Swapped: now y is column
-                int x = p.row;  // Swapped: now x is row
-                
-                if (!map.containsKey(y)) {
-                    map.put(y, new TreeMap<>());
+        Queue<Pair>q=new LinkedList<>();
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>>map=new TreeMap<>();
+        q.add(new Pair(root,0,0));
+        while(!q.isEmpty()){
+            for(int i=0;i<q.size();i++){
+                Pair p=q.poll();
+                TreeNode curr=p.node;
+                int x=p.row;
+                int y=p.col;
+                if(!map.containsKey(y)){
+                    map.put(y,new TreeMap<>());
                 }
-                if (!map.get(y).containsKey(x)) {
-                    map.get(y).put(x, new PriorityQueue<>());
+                if(!map.get(y).containsKey(x)){
+                    map.get(y).put(x,new PriorityQueue<>());
                 }
-                
-                map.get(y).get(x).offer(currNode.val);
-                
-                if (currNode.left != null) {
-                    q.offer(new Pair(currNode.left, x + 1, y - 1)); // Move left: x+1, y-1
+                map.get(y).get(x).offer(curr.val);
+                if(curr.left!=null){
+                    q.add(new Pair(curr.left,x+1,y-1));
                 }
-                
-                if (currNode.right != null) {
-                    q.offer(new Pair(currNode.right, x + 1, y + 1)); // Move right: x+1, y+1
+                if(curr.right!=null){
+                    q.add(new Pair(curr.right,x+1,y+1));
                 }
             }
         }
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        for (TreeMap<Integer, PriorityQueue<Integer>> key : map.values()) {
-            List<Integer> currList = new ArrayList<>();
-            for (PriorityQueue<Integer> nodes : key.values()) {
-                while (!nodes.isEmpty()) {
-                    currList.add(nodes.poll());
+        List<List<Integer>>ans=new ArrayList<>();
+        for(TreeMap<Integer,PriorityQueue<Integer>>key:map.values()){
+            List<Integer>currlist=new ArrayList<>();
+            for(PriorityQueue<Integer>pq:key.values()){
+                while(!pq.isEmpty()){
+                    currlist.add(pq.poll());
                 }
             }
-            ans.add(new ArrayList<>(currList));
+            ans.add(new ArrayList<>(currlist));
         }
         return ans;
     }
