886d6b31150f3034699f98967ac1ce9e251a58d6
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-11 15:19:22 +0530

    Time: 4 ms (51.50%) | Memory: 51.8 MB (25.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,6 +1,6 @@
 class Solution {
     public void dfs(char[][]grid, int row,int col){
-        grid[row][col]=0;
+        grid[row][col]='0';
         int[] drow={0,-1,0,1};
         int[] dcol={-1,0,1,0};
         for(int i=0;i<4;i++){
@@ -19,10 +19,30 @@ class Solution {
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     cnt++;
-                    dfs(grid,i,j);
+                     dfs(grid,i,j);
+                    //bfs(grid,i,j);
                 }
             }
         }
         return cnt;
+    }
+    public void bfs(char[][] grid ,int row,int col){
+        Queue<int []>q=new LinkedList<>();
+        q.add(new int[]{row,col});
+        int[] drow={0,-1,0,1};
+        int[] dcol={-1,0,1,0};
+        grid[row][col]='0';
+        while(!q.isEmpty()){
+            int []curr=q.poll();
+            for(int i=0;i<4;i++){
+            int nrow=curr[0]+drow[i];
+            int ncol=curr[1]+dcol[i];
+            if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length && grid[nrow][ncol]=='1'){
+                q.add(new int[]{nrow,ncol});
+                grid[nrow][col]='0';
+            }
+        }
+        }
+       
     }
 }
