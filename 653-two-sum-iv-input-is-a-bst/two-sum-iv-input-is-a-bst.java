fbd680804ce40f5a5712c1f2b3de8d9a2fc757b3
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-11 11:10:58 +0530

    Time: 2 ms (96.12%) | Memory: 45.6 MB (12.64%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,59 +1,63 @@
-class BSTIterator {
-Stack<TreeNode> s = new Stack<>();
-Boolean reverse = true;
-public BSTIterator(TreeNode root,Boolean reverse) {
-    this.reverse = reverse;
-    pushAll(root);
-}
-public int next() {
-    TreeNode temp = s.pop();
-    if(reverse == false){
-        pushAll(temp.right);
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator{
+    Stack<TreeNode>st=new Stack<>();
+    Boolean reverse=true;
+    public BSTIterator(TreeNode root,Boolean reverse){
+        this.reverse=reverse;
+        pushAll(root);
     }
-    else{
-        pushAll(temp.left);
+    public int next(){
+        TreeNode temp=st.pop();
+        if(reverse==false){
+            pushAll(temp.right);
+        }else{
+            pushAll(temp.left);
+        }
+        return temp.val;
     }
-    return temp.val;
-    
-}
+    public boolean hasnext(){
+        return !st.isEmpty();
+    }
+    public void pushAll(TreeNode node){
+        if(reverse==false){
+            while(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+        }else{
+            while(node!=null){
+                st.push(node);
+                node=node.right;
+            }
+        }
+    }
 
-//This function just tells whether there are any elements left or not.
-public boolean hasNext() {
-    return !s.isEmpty();
-}
-public void pushAll(TreeNode node){
-    
-    if(reverse == false){
-        while(node!=null){
-        s.push(node);
-        node = node.left;
-    }
-}
-    else{
-        while(node!=null){
-        s.push(node);
-        node = node.right;
-		}
-	}
-}
 }
 class Solution {
-public boolean findTarget(TreeNode root, int k) {
-
-	//Creating an instance of the BSTIterator class to get the desired traversal.
-    BSTIterator left = new BSTIterator(root,false);
-    BSTIterator right = new BSTIterator(root,true);
-    
-    int i = left.next();
-    int j = right.next();
-    
-	//Using simple binary search logic to find the answer.
-    while(i<j){
-        if(i+j == k) return true;
-        else if(i+j< k) i = left.next();
-        else j =right.next();
+    public boolean findTarget(TreeNode root, int k) {
+        BSTIterator left=new BSTIterator(root,false);
+        BSTIterator right=new BSTIterator(root,true);
+        int i=left.next();
+        int j=right.next();
+        while(i<j){
+            if(i+j==k) return true;
+            else if(i+j<k) i=left.next();
+            else j=right.next();
         }
-    return false;
+        return false;
     }
 }
-
