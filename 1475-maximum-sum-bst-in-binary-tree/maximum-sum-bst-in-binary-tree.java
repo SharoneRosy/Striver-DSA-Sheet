af9cc584ba2e0c46b44ed914723c9b6a187e33c0
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-11 11:51:39 +0530

    Time: 7 ms (56.29%) | Memory: 59.8 MB (43.97%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.java
@@ -13,55 +13,36 @@
  *     }
  * }
  */
-
- class NodeValue{
-    int maxValue;
-    int minValue;
+class Node{
+    int max;
+    int min;
     int size;
     int maxSum;
-    NodeValue(int maxValue,int minValue,int size,int maxSum){
-        this.maxValue=maxValue;
-        this.minValue=minValue;
+    Node(int max,int min,int size,int maxSum){
+        this.max=max;
+        this.min=min;
         this.size=size;
         this.maxSum=maxSum;
     }
- }
+}
 class Solution {
-    int max=0;
+    int maxi=0;
     public int maxSumBST(TreeNode root) {
-        //Why this didnt work .Because in the tree if both positive and negative numbers present.
-        //Negative shoud be ignored and set to zero.How to do use Math.max
-        /*int maxSum=maxSumBstHelper(root).maxSum;
-        if(maxSum>0){
-            return maxSum;
-        }else{
-            return 0;
-        }*/
-        maxSumBstHelper(root);
-        return max;
-
+        solve(root);
+        return maxi;
     }
-
-    public NodeValue maxSumBstHelper(TreeNode root){
-        //if tree is empty return 0 size
+    public Node solve(TreeNode root){
         if(root==null){
-            return new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE,0,0);
+            return new Node(Integer.MIN_VALUE,Integer.MAX_VALUE,0,0);
         }
-        NodeValue left=maxSumBstHelper(root.left);
-        NodeValue right=maxSumBstHelper(root.right);
-
-        //If current node is greater than largest in left and smaller than smallest in right
-        //it is valid bst
-        if(left.maxValue<root.val && root.val<right.minValue){
-            //it is valid bst
-            max=Math.max(max,root.val+left.maxSum+right.maxSum);
-            return new NodeValue(Math.max(root.val,right.maxValue),Math.min(root.val,left.minValue),
-                                (1+left.size+right.size),(root.val+left.maxSum+right.maxSum));
+        Node left=solve(root.left);
+        Node right=solve(root.right);
+        if(left.max<root.val && root.val<right.min){
+            maxi=Math.max(maxi,root.val+left.maxSum+right.maxSum);
+            return new Node(Math.max(root.val,right.max),Math.min(root.val,left.min),
+            (1+left.size+right.size),(root.val+left.maxSum+right.maxSum));
         }
-
-        //if invalid BST.pass such values that it cannot pass in future.
-        return new NodeValue(Integer.MAX_VALUE,Integer.MIN_VALUE,
-                                Math.max(left.size,right.size),Math.max(left.maxSum,right.maxSum));
-
+        return new Node(Integer.MAX_VALUE,Integer.MIN_VALUE,
+                Math.max(left.size,right.size),Math.max(left.maxSum,right.maxSum));
     }
 }
