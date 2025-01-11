628303a14fce61447895b8d24df47580aa5a4359
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-11 09:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (84.93%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,13 +13,14 @@
  *     }
  * }
  */
-class Solution {
-    public boolean solve(TreeNode root,long min,long max){
+
+class Solution{
+    public boolean solve(TreeNode root, long min,long max){
         if(root==null) return true;
         if(root.val>=max || root.val<=min) return false;
         return solve(root.left,min,root.val) && solve(root.right,root.val,max);
     }
-    public boolean isValidBST(TreeNode root) {
+    public boolean isValidBST(TreeNode root){
         return solve(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
 }
