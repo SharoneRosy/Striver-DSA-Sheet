644c6d212556138a4d66b511dc4f22a2aa0e25e7
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-14 15:35:39 +0530

    Time: 14 ms (37.68%) | Memory: 44.6 MB (35.38%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,76 +1,77 @@
-
-class DisjointSet {
-    private int[] size;
-    private int[] parent;
-
-    public DisjointSet(int n) {
-        size = new int[n + 1];
-        parent = new int[n + 1];
-
-        Arrays.fill(size, 1); // to take care of 1-based indexing as well
-
-        for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
-
-    public int findParent(int u) {
-        if (u == parent[u])
-            return u;
-        return parent[u] = findParent(parent[u]);
-    }
-
-    public void unionBySize(int u, int v) {
-        int up_u = findParent(u);
-        int up_v = findParent(v);
-
-        if (up_u == up_v)
-            return;
-
-        if (size[up_u] > size[up_v]) {
-            parent[up_v] = up_u;
-            size[up_u] += size[up_v];
-        } else {
-            parent[up_u] = up_v;
-            size[up_v] += size[up_u];
-        }
-    }
-}
+import java.util.*;
 
 class Solution {
     public int swimInWater(int[][] grid) {
-        HashMap<Integer, int[]> map = new HashMap<>();
         int n = grid.length;
+        DisjointSet dsu = new DisjointSet(n * n);
 
-        if (n == 1)
-            return 0;
-
-        DisjointSet ds = new DisjointSet(n * n);
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                map.put(grid[i][j], new int[]{i, j});
-
-        int time = 0;
-
-        while (ds.findParent(0) != ds.findParent(n * n - 1)) {
-            int[] node = map.get(time);
-            int x = node[0];
-            int y = node[1];
-            int[] dx = {-1, 0, 1, 0};
-            int[] dy = {0, -1, 0, 1};
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = x + dx[i];
-                int ncol = y + dy[i];
-
-                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < n && grid[nrow][ncol] <= time)
-                    ds.unionBySize(nrow * n + ncol, x * n + y);
+        // Priority queue to process cells in increasing order of elevation
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> grid[a[0]][a[1]] - grid[b[0]][b[1]]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                pq.add(new int[]{i, j});
             }
-
-            time++;
         }
 
-        return time - 1;
+        // Directions for 4-way movement
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+
+        while (!pq.isEmpty()) {
+            int[] cell = pq.poll();
+            int x = cell[0], y = cell[1];
+            int time = grid[x][y];
+
+            // Union this cell with its adjacent cells if they are within time constraints
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] <= time) {
+                    dsu.union(x * n + y, nx * n + ny);
+                }
+            }
+
+            // If the top-left (0,0) and bottom-right (n-1,n-1) are connected, return the current time
+            if (dsu.find(0) == dsu.find(n * n - 1)) {
+                return time;
+            }
+        }
+
+        return -1; // This line should never be reached
+    }
+}
+
+class DisjointSet {
+    int[] parent, size;
+
+    public DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int node) {
+        if (node != parent[node]) {
+            parent[node] = find(parent[node]); // Path compression
+        }
+        return parent[node];
+    }
+
+    public void union(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+
+        if (rootU != rootV) {
+            if (size[rootU] < size[rootV]) {
+                parent[rootU] = rootV;
+                size[rootV] += size[rootU];
+            } else {
+                parent[rootV] = rootU;
+                size[rootU] += size[rootV];
+            }
+        }
     }
 }
