ac048064d437bbd2cfa1d9f09841bda35ce0ee98
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-14 10:35:54 +0530

    Time: 68 ms (80.18%) | Memory: 46.1 MB (78.75%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,39 +1,35 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            Set<String> set = new HashSet<>(wordList);
-    
-    if(!set.contains(endWord)) return 0;
-    
-    Queue<String> q = new LinkedList<>();
-    q.add(beginWord);
-    
-    int res = 1;
-    
-    while(!q.isEmpty()) {
-        int size = q.size();
-        
-        for(int i = 0; i < size; i++) {
-            String word = q.poll();
-             if(word.equals(endWord)) return res;
-            
-            for(int j = 0; j < word.length(); j++){
-                for(char k = 'a'; k <= 'z'; k++)  {
-                    char arr[] = word.toCharArray();
-                    arr[j] = k;
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
 
-                    String str = new String(arr);
-                    
-                    if(set.contains(str)) {
-                        q.add(str);
-                        set.remove(str);
+        Queue<String> q = new LinkedList<>();
+        q.add(beginWord);
+        int steps = 1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String word = q.poll();
+                if (word.equals(endWord)) return steps;
+
+                char[] wordChars = word.toCharArray();
+                for (int j = 0; j < wordChars.length; j++) {
+                    char originalChar = wordChars[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        wordChars[j] = c;
+                        String newWord = new String(wordChars);
+                        if (wordSet.contains(newWord)) {
+                            q.add(newWord);
+                            wordSet.remove(newWord);
+                        }
                     }
+                    wordChars[j] = originalChar; // Restore original character
                 }
             }
+            steps++;
         }
-        
-        ++res;
+
+        return 0;
     }
-    
-    return 0;    
 }
-    }
