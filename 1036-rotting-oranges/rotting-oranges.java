52ce0d52756dd07bbf49c439f2a26b24a9d459ad
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-14 09:44:28 +0530

    Time: 2 ms (71.57%) | Memory: 42.2 MB (80.89%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,42 +1,42 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        Queue<int[]> rotten=new LinkedList<>();
-        int freshCount =0;
+        int n=grid.length;
+        int m=grid[0].length;
+        Queue<int []>q=new LinkedList<>();
+        int freshcnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    rotten.offer(new int[]{i,j});
+                    q.add(new int[]{i,j});
                 }
-                if(grid[i][j]==1)freshCount++;
+                if(grid[i][j]==1){
+                    freshcnt++;
+                }
             }
         }
-        int rottenTime=0;
-        int madeRotten=0;
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        while(!rotten.isEmpty()&& freshCount>0){
-            rottenTime++;
-            int queueSize = rotten.size();
-            for(int i=0;i<queueSize;i++){
-                int[] curr = rotten.poll();
+        int time=0;
+        int maderotten=0;
+        int drow[]={0,-1,0,1};
+        int dcol[]={-1,0,1,0};
+        while(!q.isEmpty() && freshcnt>0){
+            time++;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int []curr=q.poll();
                 int x=curr[0];
-                int y = curr[1];
-                    for(int j=0;j<4;j++){
-                        int nx=x+dir[j][0];
-                        int ny=y+dir[j][1];
-                        if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1){
-                            grid[nx][ny]=2;
-                            rotten.offer(new int[]{nx,ny});
-                            freshCount--;
-                        }
-                        
+                int y=curr[1];
+                for(int k=0;k<4;k++){
+                    int nrow=x+drow[k];
+                    int ncol=y+dcol[k];
+                    if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1){
+                        grid[nrow][ncol]=2;
+                        q.add(new int[]{nrow,ncol});
+                        freshcnt--;
                     }
-                
+                }
             }
-        
         }
-        return freshCount==0 ? rottenTime:-1 ;
+        return freshcnt==0?time:-1;
     }
 }
