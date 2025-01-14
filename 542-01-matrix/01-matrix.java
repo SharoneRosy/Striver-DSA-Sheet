51bbc4f8e208b26f85ff0b5c83ece7a8a8af1d98
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-14 10:03:35 +0530

    Time: 12 ms (92.22%) | Memory: 47.1 MB (68.70%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,55 +1,40 @@
-class Pair
-{
-  int row;
-  int col;
-  int step;
-  Pair(int r,int c,int s)
-  {
-      this.row=r;
-      this.col=c;
-      this.step=s;
-  }
-}
-class Solution 
-{
-    public int[][] updateMatrix(int[][] mat) 
-    {
-        int n=mat.length;
-        int m=mat[0].length;
-        boolean vis[][]=new boolean[n][m];
-        Queue<Pair>q=new LinkedList<>();
-        int ans[][]=new int[n][m];
-        for(int i=0;i<n;i++)
-        {
-          for(int j=0;j<m;j++)
-          {
-              if(mat[i][j]==0)
-              {
-                  q.add(new Pair(i,j,0));
-                  vis[i][j]=true;
-              }
-          }
-        }
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
-        while(!q.isEmpty())
-        {
-            int r=q.peek().row;
-            int c=q.peek().col;
-            int s=q.peek().step;
-            ans[r][c]=s;
-            q.poll();
-           for(int i=0;i<4;i++)
-           {
-            int nrow=r+delrow[i];
-            int ncol=c+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==false)
-             {
-                 vis[nrow][ncol]=true;
-                 q.add(new Pair(nrow,ncol,s+1));
-             }
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        int drow[] = {1, -1, 0, 0};
+        int dcol[] = {0, 0, 1, -1};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    q.add(new int[]{i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
             }
         }
-        return ans;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int row = curr[0];
+                int col = curr[1];
+
+                for (int k = 0; k < 4; k++) {
+                    int nrow = row + drow[k];
+                    int ncol = col + dcol[k];
+                    if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && mat[nrow][ncol] == -1) {
+                        mat[nrow][ncol] = mat[row][col] + 1;
+                        q.add(new int[]{nrow, ncol});
+                    }
+                }
+            }
+        }
+
+        return mat;
     }
 }
