84d8f279a869116c132578a2bda808accff6df4c
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-15 16:48:49 +0530

    Time: 14 ms (74.03%) | Memory: 45.1 MB (58.12%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,29 +1,31 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] map = new int[128];
+        int n = s.length(), m = t.length();
+        int minLength = Integer.MAX_VALUE, count = 0;
+        int left = 0, right = 0, startIndex = -1;
+        HashMap<Character, Integer> seen = new HashMap<>();
+
         for (char c : t.toCharArray()) {
-            map[c] += 1;
+            seen.put(c, seen.getOrDefault(c, 0) + 1);
         }
-        int begin = 0;
-        int len = Integer.MAX_VALUE;
-        int count = t.length();
-        for (int left=0, right=0; right<s.length(); right++) {
-            char c = s.charAt(right);
-            map[c]--;
-            if(map[c]>=0) count--; 
-            while (count == 0) {
-                char lc = s.charAt(left);
-                map[lc]++;
-                if (map[lc]>0) {
-                    if (right-left+1<len) {
-                        begin = left;
-                        len = right-left+1;
-                    }
-                    count++;
+
+        while (right < n) {
+            if (seen.getOrDefault(s.charAt(right), 0) > 0) count++;
+            seen.put(s.charAt(right), seen.getOrDefault(s.charAt(right), 0) - 1);
+
+            while (count == m) {
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    startIndex = left;
                 }
+
+                seen.put(s.charAt(left), seen.getOrDefault(s.charAt(left), 0) + 1);
+                if (seen.get(s.charAt(left)) > 0) count--;
                 left++;
             }
+            right++;
         }
-        return len==Integer.MAX_VALUE?"":s.substring(begin, begin+len);
+
+        return (startIndex == -1) ? "" : s.substring(startIndex, startIndex + minLength);
     }
 }
