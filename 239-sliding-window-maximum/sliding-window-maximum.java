40bb056942f6f2af0f4bfedc2a096ecfc30ff88f
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-15 17:17:42 +0530

    Time: 28 ms (90.02%) | Memory: 58.7 MB (65.17%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -2,30 +2,26 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
         int res[]=new int[n-k+1];
-        Deque<Integer> dq=new LinkedList<>();
-        int i=0,j=0;
+        int l=0,r=0;
         int t=0;
-        while(j<n)
-        {
-            // Calculation
-            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[j])
-            {
+        Deque<Integer>dq=new LinkedList<>();
+        while(r<n){
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[r]){
                 dq.pollLast();
             }
-            dq.offerLast(j);
-            
-            // when  the window size is equal to K
-            if(j-i+1==k)
-            {
-                // Using the Calculation to take out the ans
+            dq.addLast(r);
+
+            if(r-l+1==k){
                 res[t++]=nums[dq.peekFirst()];
-                i++;
-                if(dq.peekFirst()<i)
+                l++;
+                if(dq.peekFirst()<l){
                     dq.pollFirst();
+                }
             }
-            //when we don't reach the window size just increment j
-            if((j-i+1)<k)
-                j++;
+
+            if(r-l+1<k){
+                r++;
+            }
         }
         return res;
     }
