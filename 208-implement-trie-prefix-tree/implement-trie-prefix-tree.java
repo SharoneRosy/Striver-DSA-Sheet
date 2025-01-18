4a7d9560d220244e174ea7ae06f7d54b70581a17
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-18 08:20:36 +0530

    Time: 39 ms (41.36%) | Memory: 55.5 MB (54.60%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,78 +1,50 @@
-class TrieNode{
-    private TrieNode[] links;
-    private boolean isEnd;
 
-    public TrieNode(){
-        links=new TrieNode[26];
-    }
-
-    public boolean containsKey(char ch){
-        return links[ch-'a']!=null;
-    }
-
-    public TrieNode get(char ch){
-        return links[ch-'a'];
-    }
-
-    public void put(char ch,TrieNode node){
-        links[ch-'a']=node;
-    }
-
-    public void setEnd(){
-        isEnd=true;
-    }
-    
-    public boolean isEnd(){
-        return isEnd;
-    }
-
+ public class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean endOfWord = false;
 }
+
 class Trie {
+
     private TrieNode root;
 
     public Trie() {
-        root=new TrieNode();
+        root = new TrieNode();
     }
-    
+
     public void insert(String word) {
-        TrieNode node=root;
-        for(int i=0;i<word.length();i++){
-            char c=word.charAt(i);
-            if(!node.containsKey(c)){
-                node.put(c,new TrieNode());
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                cur.children[i] = new TrieNode();
             }
-            node=node.get(c);
+            cur = cur.children[i];
         }
-        node.setEnd();
+        cur.endOfWord = true;
     }
-    
-    private TrieNode searchPrefix(String word){
-        TrieNode node=root;
-        for(int i=0;i<word.length();i++){
-            char c=word.charAt(i);
-            if(node.containsKey(c)){
-                node=node.get(c);
-            }else{
-                return null;
-            }
-        }
-        return node;
-    }
+
     public boolean search(String word) {
-        TrieNode node=searchPrefix(word);
-        return node!=null && node.isEnd();
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                return false;
+            }
+            cur = cur.children[i];
+        }
+        return cur.endOfWord;
     }
-    
+
     public boolean startsWith(String prefix) {
-        TrieNode node=searchPrefix(prefix);
-        return node!=null;
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                return false;
+            }
+            cur = cur.children[i];
+        }
+        return true;
     }
 }
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
