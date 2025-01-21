4588c34f1f2b6d12c46221f7a5ace4e106dc892c
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-21 10:48:54 +0530

    Time: 6 ms (69.58%) | Memory: 45.2 MB (82.03%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,32 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        int [] dom = null;
-        Stack<Integer> ans = new Stack();
-        for(int i = 0;i < asteroids.length; i++){
-            if(!ans.isEmpty() && asteroids[i] < 0){
-                if(ans.peek() == Math.abs(asteroids[i])){
-                    ans.pop();
-                    continue;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+            // Process each asteroid
+            while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
+                int diff = a + stack.peek();
+                if (diff < 0) {
+                    // Current asteroid is stronger, remove the top of the stack
+                    stack.pop();
+                } else if (diff > 0) {
+                    // Top of the stack is stronger, ignore current asteroid
+                    a = 0;
+                } else {
+                    // Both asteroids destroy each other
+                    stack.pop();
+                    a = 0;
                 }
-                while(!ans.isEmpty() && ans.peek() >0 && ans.peek()< Math.abs(asteroids[i])){
-                    ans.pop();
-                }
-                if( !ans.isEmpty()&&ans.peek() > 0){
-                    if(ans.peek() == Math.abs(asteroids[i])){
-                        ans.pop();
-                    }
-                    
-                }else{
-                    ans.add(asteroids[i]);
-                }
-            }else{
-                ans.add(asteroids[i]);
+            }
+            if (a != 0) {
+                // If the current asteroid survives, add it to the stack
+                stack.push(a);
             }
         }
-        dom = new int[ans.size()];
-        for(int i =0;i<ans.size();i++){
-            dom[i] = ans.get(i);
+
+        // Convert the stack to an array
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
         }
-        return dom;
+
+        return result;
     }
 }
