27567cce64426bcf0ff81a743358b86164d0b6ad
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-28 09:23:44 +0530

    Time: 2 ms (69.08%) | Memory: 45.2 MB (17.39%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,34 +1,26 @@
 class Solution {
-    public int bfs(int[][] grid,int i,int j,int n,int m){
-        Queue<int []>q=new LinkedList<>();
+    public int dfs(int [][]grid,int i,int j,int m,int n){
+        int cnt=1;
         grid[i][j]=0;
         int dr[]={-1,0,1,0};
         int dc[]={0,1,0,-1};
-        int res=1;
-        q.add(new int[]{i, j});
-        while(!q.isEmpty()){
-             int []curr=q.poll();
-             int r=curr[0],c=curr[1];
-             for(int k=0;k<4;k++){
-                int nrow=r+dr[k];
-                int ncol=c+dc[k];
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1){
-                    res++;
-                    q.add(new int[]{nrow,ncol});
-                    grid[nrow][ncol]=0;
-                }
-           }
+        for(int k=0;k<4;k++){
+            int nrow=i+dr[k];
+            int ncol=j+dc[k];
+            if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && grid[nrow][ncol]==1){
+                cnt+=dfs(grid,nrow,ncol,m,n);
+            }
         }
-        return res;
+        return cnt;
     }
     public int maxAreaOfIsland(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
+        int m=grid.length;
+        int n=grid[0].length;
         int max=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                   max=Math.max(max,bfs(grid,i,j,n,m));
+                    max=Math.max(max,dfs(grid,i,j,m,n));
                 }
             }
         }
