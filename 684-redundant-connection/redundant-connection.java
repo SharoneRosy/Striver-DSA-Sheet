f53c171a91e1e602405ccbfe3094ed6992762a98
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-01-29 13:48:25 +0530

    Time: 1 ms (75.21%) | Memory: 43 MB (93.34%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n=edges.length+1;
         DisjointSet ds=new DisjointSet(n);
-        for(int [] edge:edges){
+        for(int []edge:edges){
             int u=edge[0];
             int v=edge[1];
-            if(ds.find(u)==ds.find(v)){
+            if(ds.find(v)==ds.find(u)){
                 return edge;
             }else{
                 ds.union(u,v);
@@ -15,15 +15,15 @@ class Solution {
     }
 }
 class DisjointSet{
-    int[] parent;
-    int[] size;
+    int []parent;
+    int []size;
     public DisjointSet(int n){
         parent=new int[n];
         size=new int[n];
         for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
+        parent[i]=i;
+        size[i]=1;
+      }
     }
     public int find(int node){
         if(node!=parent[node]){
@@ -44,4 +44,5 @@ class DisjointSet{
             }
         }
     }
+
 }
