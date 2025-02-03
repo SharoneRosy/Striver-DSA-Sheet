2e6f3d003e9274c89860940c1019f42bf351c56e
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-02-03 18:24:55 +0530

    Time: 1 ms (93.94%) | Memory: 43.3 MB (9.43%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,38 +1,20 @@
 class Solution {
-
     public int longestMonotonicSubarray(int[] nums) {
-        int maxLength = 0;
-
-        // Find longest strictly increasing subarray
-        for (int start = 0; start < nums.length; start++) {
-            int currLength = 1;
-            for (int pos = start + 1; pos < nums.length; pos++) {
-                // Extend subarray if next element is larger
-                if (nums[pos] > nums[pos - 1]) {
-                    currLength++;
-                } else {
-                    // Break if sequence is not increasing anymore
-                    break;
-                }
+        int i=1,d=1;
+        int max=1;
+        for(int j=0;j<nums.length-1;j++){
+            if(nums[j]<nums[j+1]) {
+                i++;
+                d=1;
+            }else if(nums[j]>nums[j+1]){
+                d++;
+                i=1;
+            }else{
+                i=1;
+                d=1;
             }
-            maxLength = Math.max(maxLength, currLength);
+            max=Math.max(max,Math.max(i,d));
         }
-
-        // Find longest strictly decreasing subarray
-        for (int start = 0; start < nums.length; start++) {
-            int currLength = 1;
-            for (int pos = start + 1; pos < nums.length; pos++) {
-                // Extend subarray if next element is smaller
-                if (nums[pos] < nums[pos - 1]) {
-                    currLength++;
-                } else {
-                    // Break if sequence is not decreasing anymore
-                    break;
-                }
-            }
-            maxLength = Math.max(maxLength, currLength);
-        }
-
-        return maxLength; // Return the longer of increasing or decreasing sequences
+        return max;
     }
 }
