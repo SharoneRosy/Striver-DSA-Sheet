e4056f2363e1bc1bc5553263bde401c02bad7631
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-02-06 20:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (38.69%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,13 @@
-public class Solution {
-    public int climbStairs(int n) {
-        int memo[] = new int[n + 1];
-        return climb_Stairs(0, n, memo);
+class Solution {
+    public int solve(int i,int n,int []dp){
+        if(i>n) return 0;
+        if(i==n ) return 1;
+        if(dp[i]>0) return dp[i];
+        dp[i]=solve(i+1,n,dp)+solve(i+2,n,dp);
+        return dp[i];
     }
-
-    public int climb_Stairs(int i, int n, int memo[]) {
-        if (i > n) {
-            return 0;
-        }
-        if (i == n) {
-            return 1;
-        }
-        if (memo[i] > 0) {
-            return memo[i];
-        }
-        memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
-        return memo[i];
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        return solve(0,n,dp);
     }
 }
