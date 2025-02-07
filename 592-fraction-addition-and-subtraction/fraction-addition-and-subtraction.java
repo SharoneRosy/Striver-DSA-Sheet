d9cd383369981dbe1c4347d2ed9f5cc45f4659ef
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-02-07 21:30:37 +0530

    Time: 4 ms (97.61%) | Memory: 42.1 MB (52.63%) - LeetSync

--- 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
+++ 592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
@@ -1,69 +1,48 @@
 class Solution {
     public String fractionAddition(String expression) {
-        int n = expression.length();
-        int c = 0;
-        for(int i=0;i<n;i++){
-            if(expression.charAt(i)=='/'){
-                c++;
-            }
-        }
-        int[] num = new int[c];
-        int[] den = new int[c];
-        int[] s = new int[c];
-        int i = 0;
-        if(expression.charAt(0)=='-'){
-            s[0]=-1;
-            i=1;
-        }
-        else{
-            s[0]=1;
-        }
-        int j=0,k=0,l=1;
+        int numerator = 0, denominator = 1; // Start with 0/1
+
+        int i = 0, n = expression.length();
         while (i < n) {
+            // Read sign
+            int sign = 1;
             if (expression.charAt(i) == '-' || expression.charAt(i) == '+') {
-                if (expression.charAt(i) == '-') {
-                    s[l] = -1;
-                } else {
-                    s[l] = 1;
-                }
-                l++;
+                sign = (expression.charAt(i) == '-') ? -1 : 1;
                 i++;
             }
-            int nu=0;
+
+            // Read numerator
+            int num = 0;
             while (i < n && Character.isDigit(expression.charAt(i))) {
-                nu=nu*10+(expression.charAt(i)-'0');
+                num = num * 10 + (expression.charAt(i) - '0');
                 i++;
             }
-            num[j++] = nu;
-            i++; 
-            int de = 0;
+
+            // Skip '/'
+            i++;
+
+            // Read denominator
+            int denom = 0;
             while (i < n && Character.isDigit(expression.charAt(i))) {
-                de=de* 10+(expression.charAt(i)-'0');
+                denom = denom * 10 + (expression.charAt(i) - '0');
                 i++;
             }
-            den[k++]=de;
+
+            // Update fraction
+            numerator = numerator * denom + sign * num * denominator;
+            denominator *= denom;
+
+            // Reduce fraction
+            int gcd = gcd(Math.abs(numerator), denominator);
+            numerator /= gcd;
+            denominator /= gcd;
         }
-        int lcm = den[0];
-        for(i=1;i<c;i++){
-            lcm = lcm*(den[i]/helper(lcm,den[i]));;
-        }
-        for(i=0;i<c;i++){
-            int a = (lcm/den[i])*s[i];
-            num[i]*=a;
-        }
-        int sum=0;
-        for(i=0;i<c;i++){
-            sum+=num[i];
-        }
-        int x = helper(Math.abs(sum),lcm);
-        sum=sum/x;
-        lcm=lcm/x;
-        String str = Integer.toString(sum)+"/"+Integer.toString(lcm);
-        return str;
+
+        return numerator + "/" + denominator;
     }
-    public static int helper(int a,int b) {
-        if (b==0)
-            return a;
-        return helper(b, a % b);
+
+    // Helper function to calculate GCD
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
