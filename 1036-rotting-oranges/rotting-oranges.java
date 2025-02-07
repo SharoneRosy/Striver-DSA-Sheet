4d9d7798c04a8a35165bf375483c8a738266aa76
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-02-07 12:50:57 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (74.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,41 +2,38 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
+        int fresh=0;
         Queue<int []>q=new LinkedList<>();
-        int freshcnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.add(new int[]{i,j});
-                }
-                if(grid[i][j]==1){
-                    freshcnt++;
+                }else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-
+        int []dr={-1,0,1,0};
+        int []dc={0,1,0,-1};
         int time=0;
-        int maderotten=0;
-        int drow[]={0,-1,0,1};
-        int dcol[]={-1,0,1,0};
-        while(!q.isEmpty() && freshcnt>0){
-            time++;
+        while(!q.isEmpty() && fresh>0){
             int size=q.size();
             for(int i=0;i<size;i++){
-                int []curr=q.poll();
-                int x=curr[0];
-                int y=curr[1];
+                int curr[]=q.poll();
+                int r=curr[0];
+                int c=curr[1];
                 for(int k=0;k<4;k++){
-                    int nrow=x+drow[k];
-                    int ncol=y+dcol[k];
+                    int nrow=r+dr[k];
+                    int ncol=c+dc[k];
                     if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol]==1){
                         grid[nrow][ncol]=2;
+                        fresh--;
                         q.add(new int[]{nrow,ncol});
-                        freshcnt--;
                     }
                 }
             }
+            time++;
         }
-        return freshcnt==0?time:-1;
+        return fresh==0?time:-1;
     }
 }
