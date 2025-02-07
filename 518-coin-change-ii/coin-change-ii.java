27cde2b41c4f137b49f9bd9abbd1d5893b807c60
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-02-07 15:11:47 +0530

    Time: 9 ms (63.60%) | Memory: 49.7 MB (18.21%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,48 +1,25 @@
 class Solution {
-    //Recursion
-    // public int solve(int amount, int[] coins,int i){
-    //     if (amount==0) return 1;
-    //     if(i==0){
-    //         if(amount%coins[0]==0) return 1;
-    //         else return 0;
-    //     }
-    //     int notpick=solve(amount,coins,i-1);
-    //     int pick=0;
-    //     if(amount>=coins[i]){
-    //         pick=solve(amount-coins[i],coins,i);
-    //     }
-    //     return pick+notpick;
-    // }
-    //memoization
-    // public int solve(int amount, int[] coins,int i,int dp[][]){
-    //     if (amount==0) return 1;
-    //     if(i==0){
-    //         if(amount%coins[0]==0) return 1;
-    //         else return 0;
-    //     }
-    //     if(dp[i][amount]!=-1) return dp[i][amount];
-    //     int notpick=solve(amount,coins,i-1,dp);
-    //     int pick=0;
-    //     if(amount>=coins[i]){
-    //         pick=solve(amount-coins[i],coins,i,dp);
-    //     }
-    //     dp[i][amount]=pick+notpick;
-    //     return dp[i][amount];
-    // }
+    public int solve(int amount,int []coins,int i){
+        if(amount==0) return 1;
+        if(i==0) {
+            if(amount%coins[0]==0) return 1;
+            else return 0;
+        }
 
-
+        int notpick=solve(amount,coins,i-1);
+        int pick=0;
+        if(amount>=coins[i]){
+            pick=solve(amount-coins[i],coins,i);
+        }
+        return pick+notpick;
+    }
     // public int change(int amount, int[] coins) {
     //     int n=coins.length;
-    //     int dp[][]=new int [coins.length][amount+1];
-    //     for(int[]  row:dp){
-    //         Arrays.fill(row,-1);
-    //     }
-    //     return solve(amount,coins,n-1,dp);
+    //     // return solve(amount,coins,n-1);
     // }
-
-    public int change(int amount, int[] coins) {
+    public int change(int amount,int [] coins){
         int n=coins.length;
-        int dp[][]=new int [coins.length][amount+1];
+        int dp[][]=new int[coins.length][amount+1];
 
         for(int i=0;i<coins.length;i++){
             dp[i][0]=1;
@@ -53,14 +30,15 @@ class Solution {
         }
         for(int i=1;i<n;i++){
             for(int amt=1;amt<=amount;amt++){
-                 int notpick=dp[i-1][amt];
+                int notpick=dp[i-1][amt];
                  int pick=0;
                  if(amt>=coins[i]){
                        pick=dp[i][amt-coins[i]];
                }
-              dp[i][amt]=pick+notpick;
-             }
+            dp[i][amt]=pick+notpick;
+            }
         }
         return dp[n-1][amount];
     }
+
 }
