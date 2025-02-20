d4bcd1acd65aba64c589a7c40774ba190a4dc1f2
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-02-20 07:24:56 +0530

    Time: 46 ms (63.64%) | Memory: 113.4 MB (63.46%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,83 +1,80 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class LRUCache {
+public class LRUCache{
 
     private static final int DUMMY_KEY = -1;
 
-    // Separate ListNode class for better separation of concerns
-    static class ListNode {
-        int key, value;
-        ListNode prev, next;
+    static class ListNode{
+        int key,value;
+        ListNode prev,next;
 
-        public ListNode(int key, int value) {
-            this.key = key;
-            this.value = value;
+        public ListNode ( int key, int value) {
+            this.key=key;
+            this.value=value;
         }
     }
 
     private final int capacity;
-    private final Map<Integer, ListNode> cache;
-    private final ListNode head, tail;
+    private final Map<Integer, ListNode>cache;
+    private final ListNode head,tail;
 
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        this.cache = new HashMap<>();
-        
-        // Initialize dummy head and tail for the doubly linked list
-        this.head = new ListNode(DUMMY_KEY, DUMMY_KEY);
-        this.tail = new ListNode(DUMMY_KEY, DUMMY_KEY);
+    public LRUCache(int capacity){
+        this.capacity=capacity;
+        this.cache=new HashMap<>();
 
-        head.next = tail;
-        tail.prev = head;
+        this.head=new ListNode(DUMMY_KEY,DUMMY_KEY);
+        this.tail=new ListNode(DUMMY_KEY,DUMMY_KEY);
+
+        head.next=tail;
+        tail.prev=head;
     }
 
-    public int get(int key) {
-        if (!cache.containsKey(key)) {
+    public int get(int key){
+        if(!cache.containsKey(key)){
             return -1;
         }
-
-        ListNode node = cache.get(key);
-        moveToFront(node);  // Move accessed node to front as it is most recently used
+        ListNode node=cache.get(key);
+        moveToFront(node);
         return node.value;
     }
 
-    public void put(int key, int value) {
-        if (cache.containsKey(key)) {
-            ListNode existingNode = cache.get(key);
-            existingNode.value = value;
+    public void put(int key,int value){
+        if(cache.containsKey(key)){
+            ListNode existingNode =cache.get(key);
+            existingNode.value=value;
             moveToFront(existingNode);
-        } else {
-            ListNode newNode = new ListNode(key, value);
-            cache.put(key, newNode);
+        }else{
+            ListNode newNode=new ListNode(key,value);
+            cache.put(key,newNode);
             addToFront(newNode);
 
-            if (cache.size() > capacity) {
-                ListNode lruNode = removeLRUNode();
+            if(cache.size()>capacity){
+                ListNode lruNode=removeLRUNode();
                 cache.remove(lruNode.key);
             }
         }
     }
 
-    private void moveToFront(ListNode node) {
+    private void moveToFront(ListNode node){
         removeNode(node);
         addToFront(node);
     }
 
-    private void addToFront(ListNode node) {
-        node.next = head.next;
-        node.prev = head;
-        head.next.prev = node;
-        head.next = node;
+    private void addToFront(ListNode node){
+        node.next=head.next;
+        node.prev=head;
+        head.next.prev=node;
+        head.next=node;
     }
 
-    private void removeNode(ListNode node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
+    private void removeNode(ListNode node){
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
     }
 
-    private ListNode removeLRUNode() {
-        ListNode lruNode = tail.prev;
+    private ListNode removeLRUNode(){
+        ListNode lruNode=tail.prev;
         removeNode(lruNode);
         return lruNode;
     }
