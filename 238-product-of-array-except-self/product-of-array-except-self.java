ae7c3e820511045e16c9f5b49e2ab7e732508567
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-11-14 17:37:04 +0530

    Time: 2 ms (89.00%) | Memory: 72.1 MB (7.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,26 +1,17 @@
 class Solution {
-
     public int[] productExceptSelf(int[] nums) {
-        // The length of the input array
-        int length = nums.length;
+        int n=nums.length;
+        int ans [] =new int [n];
 
-        // Final answer array to be returned
-        int[] answer = new int[length];
-
-        answer[0] = 1;
-        for (int i = 1; i < length; i++) {
-            answer[i] = nums[i - 1] * answer[i - 1];
+        ans[0]=1;
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*nums[i-1];
         }
-
-        // R contains the product of all the elements to the right
-        // Note: for the element at index 'length - 1', there are no elements to the right,
-        // so the R would be 1
-        int R = 1;
-        for (int i = length - 1; i >= 0; i--) {
-            answer[i] = answer[i] * R;
-            R *= nums[i];
+        int postfix=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]*=postfix;
+            postfix*=nums[i];
         }
-
-        return answer;
+        return ans;
     }
 }
