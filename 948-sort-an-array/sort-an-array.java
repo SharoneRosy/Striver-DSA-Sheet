73a0ae720ef8461e52df9c4d426bd9a310caf53d
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-11-14 11:01:41 +0530

    Time: 64 ms (22.03%) | Memory: 74.7 MB (16.77%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -6,7 +6,7 @@ class Solution {
     private void mergeSort(int [] arr, int l, int r){
         if(l>=r) return ;
         int m=(l+r)/2;
-        mergeSort(arr,l, m);
+        mergeSort(arr,l,m);
         mergeSort(arr,m+1,r);
         merge(arr,l,m,r);
     }
@@ -31,8 +31,8 @@ class Solution {
             temp.add(arr[j]);
             j++;
         }
-        for( i=l;i<=r;i++){
-            arr[i]=temp.get(i-l);
+        for(int k=l;k<=r;k++){
+            arr[k]=temp.get(k-l);
         }
     }
 }
