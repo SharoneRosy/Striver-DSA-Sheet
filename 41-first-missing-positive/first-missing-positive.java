165724aa2fdce0de49168f7229b22583352feefb
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-11-17 19:56:12 +0530

    Time: 2 ms (40.18%) | Memory: 71.4 MB (10.66%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,26 +1,26 @@
 class Solution {
-
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        boolean[] seen = new boolean[n + 1]; // Array for lookup
-
-        // Mark the elements from nums in the lookup array
-        for (int num : nums) {
-            if (num > 0 && num <= n) {
-                seen[num] = true;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                nums[i]=0;
             }
         }
-
-        // Iterate through integers 1 to n
-        // return smallest missing positive integer
-        for (int i = 1; i <= n; i++) {
-            if (!seen[i]) {
+        for(int i=0;i<n;i++){
+            int val=Math.abs(nums[i]);
+            if(val>=1 && val<=n){
+                if(nums[val-1]>0){
+                    nums[val-1]*=-1;
+                }else if(nums[val-1]==0){
+                    nums[val-1]=-1*(n+1);
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(nums[i-1]>=0){
                 return i;
             }
         }
-
-        // If seen contains all elements 1 to n
-        // the smallest missing positive number is n + 1
-        return n + 1;
+        return n+1;
     }
 }
