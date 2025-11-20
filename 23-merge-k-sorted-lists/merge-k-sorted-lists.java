77bf77436fc0aab23e24083ef718dfca4d4158af
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-11-20 18:12:18 +0530

    Time: 4 ms (76.94%) | Memory: 46.6 MB (23.71%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,28 +8,25 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
-        
-        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for (ListNode list : lists) {
-            if (list != null) {
+        if(lists.length==0) return null;
+        PriorityQueue<ListNode>minHeap=new PriorityQueue<>((a,b)->a.val-b.val);
+        for(ListNode list: lists){
+            if(list!=null){
                 minHeap.offer(list);
             }
         }
+        ListNode res=new ListNode(0);
+        ListNode curr=res;
+        while(!minHeap.isEmpty()){
+            ListNode node=minHeap.poll();
+            curr.next=node;
+            curr=curr.next;
 
-        ListNode res = new ListNode(0);
-        ListNode cur = res;
-        while (!minHeap.isEmpty()) {
-            ListNode node = minHeap.poll();
-            cur.next = node;
-            cur = cur.next;
-
-            node = node.next;
-            if (node != null) {
-                minHeap.offer(node);
+            node=node.next;
+            if(node!=null){
+                minHeap.add(node);
             }
         }
         return res.next;
