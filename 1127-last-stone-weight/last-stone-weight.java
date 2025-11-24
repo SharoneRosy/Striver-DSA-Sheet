01f721e0ad20b508e88524820c979d32640cbcad
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2025-11-24 09:51:39 +0530

    Time: 1 ms (98.88%) | Memory: 42.9 MB (10.09%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,23 +1,18 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        // Max-heap to hold the stones
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        // Add all stones to the max-heap
-        for (int stone : stones) {
-            pq.add(stone);
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i:stones){
+            pq.add(i);
         }
 
-        // Simulate smashing stones
-        while (pq.size() > 1) {
-            int x = pq.poll(); // Heaviest stone
-            int y = pq.poll(); // Second heaviest stone
-            if (x != y) {
-                pq.add(x - y); // Add the remaining stone
+        while(pq.size()>1){
+            int a=pq.poll();
+            int b=pq.poll();
+            if(a!=b){
+                pq.add(a-b);
             }
         }
-
-        // If there's one stone left, return it; otherwise, return 0
-        return pq.isEmpty() ? 0 : pq.peek();
+        return (pq.isEmpty())?0:pq.peek();
     }
 }
