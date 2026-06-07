ac74262c8a314f191ab1e072c904f9936d1181e5
Sharone Rosy <103061461+SharoneRosy@users.noreply.github.com>
2026-06-07 22:40:23 +0530

    Time: 31 ms (64.14%) | Memory: 95.6 MB (39.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -5,17 +5,14 @@ class Solution {
         int max=0;
         for(int i:set){
             if(!set.contains(i-1)){
-                int len=1;
-                int x=i+1;
-                while(set.contains(x)){
-                    len++;
-                    x=x+1;
+                int cnt=1;
+                while(set.contains(i+cnt)){
+                              cnt++;
                 }
-                max=Math.max(max,len);
+                 max=Math.max(max,cnt);
             }
+           
         }
         return max;
     }
-
- 
 }
